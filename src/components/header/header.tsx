60252ea421e1c8c7f21479abda99b5ae9c8ce856
Rodrigo <rdesouza2008@gmail.com>
2023-04-02 14:35:30 -0300

    .

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container } from './styles';
 
 const Header: React.FC = () => {
-  return <Container>montserrat texto mudou</Container>;
+  return <Container>header</Container>;
 };
 
 export default Header;
